74d76d3c9ce9d1797a11e705df30d1db8d1a62b0
FrederickMih <fredtimermih@gmail.com>
2021-04-17 12:17:44 +0100

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :groups, dependent: :destroy
-  has_many :outlays, foreign_key: 'author_id', dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :avatar
+  has_many :groups, dependent: :destroy
+  has_many :outlays, foreign_key: 'author_id', dependent: :destroy       
 
+  validates :name, presence: true, uniquenesss: true, length: {maximum: 30}
+  has_one_attached :avatar, dependent: :destroy
 end
