d6b345ea62321fb2d823011fc278531fc93b0d10
FrederickMih <fredtimermih@gmail.com>
2021-04-20 14:19:48 +0100

    Modify the outlays index and new pages to stick the footer to the bottom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :password, :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
   end
+
+
+
 end
