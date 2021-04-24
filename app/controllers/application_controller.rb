7cae55b9e4d6d6cb071a0bf91f2485074bb89177
FrederickMih <fredtimermih@gmail.com>
2021-04-24 04:45:51 +0100

    Modify the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   # before_action :masquerade_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  before_action :set_cache_headers
+  def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
+  end
+
   protected
 
   def configure_permitted_parameters
