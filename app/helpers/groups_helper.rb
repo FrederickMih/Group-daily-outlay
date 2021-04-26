5fff5c1f2ac041b9f1e46ec452c1217e4a66094b
FrederickMih <fredtimermih@gmail.com>
2021-04-26 05:43:38 +0100

    Added materialized delete and edit button on group index and show pages

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -6,4 +6,25 @@ module GroupsHelper
       image_tag 'group_icon.png', class: 'group-img'
     end
   end
+
+  def edit_group_link(group)
+    return unless group.user_id == current_user.id
+    link_to '<i class="small material-icons teal-text darken-4">mode_edit</i>'.html_safe, edit_group_path(group)
+  end
+
+  def delete_group_link(group)
+     return unless group.user_id == current_user.id
+    link_to '<i class="small material-icons deep-orange-text- accent-3">delete</i>'.html_safe, group, method: :delete, date: {confirm: 'Are you sure?'}
+  end
+
+  def edit_outlay_link(outlay)
+    return unless outlay.user_id == current_user.id
+    link_to '<i class="small material-icons teal-text darken-4">mode_edit</i>'.html_safe, edit_outlay_path(group)
+  end
+
+  def delete_outlay_link(outlay)
+     return unless outlay.user_id == current_user.id
+    link_to '<i class="small material-icons deep-orange-text- accent-3">delete</i>'.html_safe, outlay, method: :delete, date: {confirm: 'Are you sure?'}
+  end
+
 end
