20bf3afe3f49c4235a048904d0282c559549068b
FrederickMih <fredtimermih@gmail.com>
2021-04-26 07:57:23 +0100

    Remove comment from codes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,30 +1,24 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[show edit update destroy]
 
-   # GET /groups/new
   def new
     @group = Group.new
   end
 
-  
 
   def index
     @groups = Group.includes(:user).paginate(page: params[:page], per_page: 3).order(:name).with_attached_icon
      @skip_footer = true
   end
 
-  # GET /groups/1 or /groups/1.json
   def show
-    # @group = Group.includes(:outlays, :user).find(params[:id])
      @skip_footer = true
      @group_outlays = @group.outlays.includes(:user)
     #  @amount = @group_outlays.pluck(:amount).sum
   end
 
-  # GET /groups/1/edit
   def edit; end
 
-  # POST /groups or /groups.json
   def create
     group = Group.new(group_params)
     group.user_id = current_user.id
@@ -39,7 +33,6 @@ class GroupsController < ApplicationController
     
   end
 
-  # PATCH/PUT /groups/1 or /groups/1.json
   def update
     respond_to do |format|
       if @group.update(group_params)
@@ -52,7 +45,6 @@ class GroupsController < ApplicationController
     end
   end
 
-  # DELETE /groups/1 or /groups/1.json
   def destroy
     @group.destroy
     respond_to do |format|
@@ -63,7 +55,6 @@ class GroupsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_group
     @group = Group.find(params[:id])
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
   def index
-    # return unless  user_signed_in?
-    # redirect_to current_user
   end
 end
