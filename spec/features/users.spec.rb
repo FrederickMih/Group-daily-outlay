c7256647884658c627b91aae750a14fe0b429b1d
FrederickMih <fredtimermih@gmail.com>
2021-04-29 03:08:38 +0100

    Includes some tests for registered users

--- spec/features/users.spec.rb
+++ spec/features/users.spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature 'User authentications', type: :feature do
-  scenario 'A new user should enter valid credentials to register' do
-    visit new_user_registration_path
-    fill_in :Name, with: 'Mih'
-    fill_in :Email, with: 'mih@gmail.com'
-    fill_in 'Password (6 characters minimum)', with: 'mypass1'
-    fill_in 'Confirm Password', with: 'mypass1'
-    click_on 'Register'
-  end
-
+   
   scenario 'should enter valid email and password to login and visit his profile page' do
     visit new_user_session_path
     fill_in :Email, with: 'mih@gmail.com'
     fill_in :Password, with: 'mypass1'
     click_on 'Login'
     visit root_path
+  end
+
+  scenario "A user with invalid email or password can't login" do
+    visit new_user_session_path
+    fill_in :Email, with: '@fred111'
+    fill_in :Password, with: 'passaaaa'
+    click_on 'Login'
+    expect(page).to have_content 'Log in'
+  end
+
+  scenario 'should visit the profile page and see his name' do
+    visit root_path
+    expect(page).to have_content 'Mih'
   end
 end
