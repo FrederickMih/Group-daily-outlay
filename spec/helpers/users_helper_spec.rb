3232edbdd392b0e4adb3d41410faeaddb93539f1
FrederickMih <fredtimermih@gmail.com>
2021-04-29 01:38:37 +0100

    Add associations tests for outlay models

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -10,11 +10,6 @@ RSpec.describe UsersHelper, type: :helper do
          it "should display default avatar if avatar is not uploaded" do
          expect(show_avatar(@user)).to eq(image_tag('avatar.jpg', class: 'avatar rounded_circle'))
          end
-
-         # it 'Should display custom avatar if avatar is provided' do
-         #    @user.avatar.attach(io: File.open('cropped.jpg'), filename: 'cropped.jpg', content_type: 'image/jpg')
-         #    expect(show_avatar(@user)).not_to eq(image_tag('avatar.jpg', class: 'avatar rounded_circle'))
-         # end
    end 
    
 end
