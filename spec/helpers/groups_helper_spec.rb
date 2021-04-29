0111fc8b61de61279a0f8830d75e2e2b8f1fc626
FrederickMih <fredtimermih@gmail.com>
2021-04-29 02:19:08 +0100

    Add test for the group display_icon method in helper

---
+++ spec/helpers/groups_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe GroupsHelper, type: :helper do
+
+   describe "display icon" do
+      before(:example) do
+            @user = User.create!(name: 'Frederick', email: 'mih@gmail.com', password: '333333')
+            @group = Group.create(name: 'MobilePhones', user_id: @user.id)
+            
+      end
+         it 'should display the default icon if an icon is not provided' do
+            expect(display_icon(@group)).to eq(image_tag('group_icon.png', class: 'group-img'))
+   end
+
+   end 
+   
+end
+
